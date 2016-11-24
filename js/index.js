2eacf13b90cb811eb74c503c2d3fc3fd8e0c07c5
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-11-24 10:34:35 +0000

    asd
    fas

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-    $ cordova plugin add org.apache.cordova.geolocation
-    $ cordova plugin ls
-    [ 'org.apache.cordova.geolocation' ]
-    $ cordova plugin rm org.apache.cordova.geolocation
   
 
 
