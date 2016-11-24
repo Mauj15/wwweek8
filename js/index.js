c4a7e252f869bcde115e20954beee41703fa599e
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-11-24 10:50:31 +0000

    error reporting

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).on('pageinit', function() {
 
 //Call this function when you want to get the current position
 function getPosition() {
-	
+	 console.log("getPostion");
 	//change time box to show updated message
 	$('#time').val("Getting data...");
 	
@@ -30,6 +30,7 @@ function getPosition() {
 
 //called when the position is successfully determined
 function successPosition(position) {
+    console.log("successPostion");
 	
 	//You can find out more details about what the position obejct contains here:
 	// http://www.w3schools.com/html/html5_geolocation.asp
@@ -38,15 +39,18 @@ function successPosition(position) {
 	//lets get some stuff out of the position object
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 	
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + time);
-	$('#lattext').val("I should contain the latitude data...");
+	$('#lattext').val(latitude);
+    $('#longtext').val(longitude);
 	
 }
 
 //called if the position is not obtained correctly
 function failPosition(error) {
+     console.log("failPostion");
 	//change time box to show updated message
 	$('#time').val("Error getting data: " + error);
 	
